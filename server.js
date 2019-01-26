9a1754d4d41793d413e4fe19e65e298afc6f9bbf
p.baluk <pawel.marcin.baluk@gmail.com>
2019-01-26 08:35:45 +0100

    build project

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
+/* eslint-disable no-undef */
 const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
 const port = process.env.PORT || 8080;
-const app = express();
-
-
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, (req, res) => {
-    res.sendfile(__dirname + "/dist/index.html");
-});
 app.listen(port);
-console.log("Server started...");
+console.log('server started '+ port);
